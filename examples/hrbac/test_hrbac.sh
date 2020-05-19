393225bff7c28bc87a9c606271601bbc9ab2f1b7
cottonty <tongy@geohey.com>
2020-05-19 17:58:27 -0400

    a cleared version of hrbac, update running script for the cmd interface of enabling rules

--- examples/hrbac/test_hrbac.sh
+++ examples/hrbac/test_hrbac.sh
@@ -1,5 +1,5 @@
 for q in $(seq 50 50 500); do
 	for mode in 'rule' 'rolerule' 'transRH' 'python' 'distalgo'; do
-		for ((n=0;n<10;n++)); do python3 -m da --message-buffer-size=409600 hrbac.da --numr=500 --numq=50 --q=$q --mode=$mode; done;
+		for ((n=0;n<1;n++)); do python3 -m da --message-buffer-size=409600 --rule hrbac.da --numr=500 --numq=50 --q=$q --mode=$mode; done;
 	done;
 done;
