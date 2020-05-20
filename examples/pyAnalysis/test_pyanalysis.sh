ff59d87a8671bfcca6f31c6d3cc7e3bc4bec5f37
cottonty <tongy@geohey.com>
2020-05-19 22:45:02 -0400

    add Loop Analysis example in pyAnalysis folder.
    update the pyanalysis test script

--- examples/pyAnalysis/test_pyanalysis.sh
+++ examples/pyAnalysis/test_pyanalysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for dataset in "numpy" "scipy" "pandas" "matplotlib" "sympy" "django" "sklearn" "torch"; do
 	for mode in "rule" "combine" "distalgo" "python"; do
-		for ((n=0;n<20;n++)); do python3 -m da ast_analysis_rule.da $dataset $mode; done;
+		for ((n=0;n<20;n++)); do python3 -m da --rule prgmAnalysis.da $dataset $mode; done;
 	done;
 done;
