6d32ee783888bea545f2f22e965076f75bbcb325
Thang Bui <thangbui1294@gmail.com>
2021-02-26 12:49:38 -0500

    update loop analyzer implementation and add python script to generate .bat file to run the loop analyzer with multiple git repos specified in a text file

---
+++ examples/pyAnalysis/run-multiple-repos.py
@@ -0,0 +1,37 @@
+repos_list_file = './data_prepare/numpy-python-most-stars-repos.txt'
+output_bat_file = './run-candidate-loop-multiple-repos.bat'
+repo_names = []
+
+with open(repos_list_file, 'r') as input_repos, open(output_bat_file, 'w') as output_bat:
+	# download repos from github
+	output_bat.write('cd data_prepare\nFOR %%A IN (')
+	for i, line in enumerate(input_repos):
+		if 'github' in line:
+			repo_parts = line.strip().split('/')
+			repo_names.append(repo_parts[-1])
+			if i == 0:
+				output_bat.write(line.strip())
+			else:
+				output_bat.write(',' + line.strip())
+	output_bat.write(') DO (\ngit clone %%A\n)\n')
+
+
+	# generate data for anlyzing
+	output_bat.write('FOR %%A IN (')
+	for i, repo_name in enumerate(repo_names):
+		if i == 0:
+			output_bat.write(repo_name)
+		else:
+			output_bat.write(',' + repo_name)
+	output_bat.write(') DO (\npython pyast_views.py %%A\n)\n')
+
+
+	# analyzing
+	output_bat.write('cd ..\nFOR %%A IN (')
+	for i, repo_name in enumerate(repo_names):
+		if i == 0:
+			output_bat.write(repo_name)
+		else:
+			output_bat.write(',' + repo_name)
+	output_bat.write(') DO (\npython -m da --rule -r launcher.da LoopToQuery %%A candidate rule\n)')
+
