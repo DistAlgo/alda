b8840d7a7ba66838fdc83b78cc70f5f6e1502107
Scott Stoller <stoller@cs.stonybrook.edu>
2022-01-19 16:34:23 -0500

    run.sh runs all PA programs on all datasets

--- examples/PA/run.sh
+++ examples/PA/run.sh
@@ -1,24 +1,82 @@
-for module in PA PAopt
-do
-  for repo in blender django matplotlib numpy pandas pytorch scikit-learn scipy sympy
-  do
-    timeout 1800 time python3.9 -m da --message-buffer-size=409600000 --rule launcher.da --data data/$repo --module $module 1>>out.txt 2>>msg.txt
-    echo "end $module-$repo" >>out.txt 
-    echo "end $module-$repo" >>msg.txt
+#!/bin/bash
 
-    for FILE in $(ls __pycache__/PA.*class_extends_rs*facts)
-    do
-      mv "$FILE" __pycache__/$repo.txt
+# run this script in examples/PA
+
+# small workload for testing
+#daPgms="PA"
+#xsbPgms="paxsb_manualopt"
+#datasets="numpy django blender matplotlib pandas pytorch scikit-learn scipy sympy"
+#startIter=0
+#endIter=0
+
+# run everything
+daPgms="PA PAopt"
+xsbPgms="paxsb paoptxsb paxsb_manualopt"
+datasets="numpy django blender matplotlib pandas pytorch scikit-learn scipy sympy"
+startIter=0
+endIter=4
+
+if [ ! -d "out" ]; then
+    mkdir out
+fi
+
+for pgm in ${daPgms}; do
+  for data in ${datasets}; do
+    for ((i=${startIter};i<=${endIter};i++)); do
+        outfile=out/${pgm}_${data}_${i}_out.txt
+        errfile=out/${pgm}_${data}_${i}_err.txt
+        rm -f $outfile $errfile
+        # remove facts files from previous runs.  
+        for FILE in $(ls __pycache__/PA.*class_extends_rs*facts 2>/dev/null); do
+            rm -f "$FILE"
+        done
+        echo "running ${pgm} ${data} ${i}"
+        # -r option to force da to re-compile, for measurement of compilation time
+        # if removing -I thread, add:  --message-buffer-size=409600000
+        timeout 1800 time python3 -m da -r -I thread --rule PAtimer.da --data data/$data --bench $pgm 1>>${outfile} 2>>${errfile}
+        # in case of timeout, append timeout message to outfile, to confirm that execution didn't abort for other reasons.
+        if [ "$?" == "124" ]; then
+            echo "timeout!" >>$outfile
+        fi
+        # rename file used as input to XSB.  I think it doesn't matter which pgm is used.
+        if [ "${pgm}" = "PA" -a "${i}" = "0" ]; then
+            for FILE in $(ls __pycache__/PA.*class_extends_rs*facts); do
+                echo "renaming ${FILE}"
+                mv "$FILE" __pycache__/$data.P
+            done
+        fi
+        if ! grep -q "run_os_total" "$outfile"; then
+            echo "incomplete iteration; skipping remaining iterations"
+            break
+        fi
     done
   done
 done
 
-for module in paxsb paoptxsb paxsb_manualopt
-do
-  for repo in blender django matplotlib numpy pandas pytorch scikit-learn scipy sympy
-  do
-    timeout 1800 time xsb -e "[$module], load_dync('__pycache__/$repo.facts'), halt."  1>>xsb_out.txt 2>>xsb_msg.txt
-    echo "end $module-$repo" >>xsb_out.txt
-    echo "end $module-$repo" >>xsb_msg.txt
+for pgm in ${xsbPgms}; do
+  for data in ${datasets}; do
+    for ((i=${startIter};i<=${endIter};i++)); do
+        outfile=out/${pgm}_${data}_${i}_out.txt
+        errfile=out/${pgm}_${data}_${i}_err.txt
+        rm -f $outfile $errfile xsb_result.txt
+        echo "running ${pgm} ${data} ${i}"
+        # redirect stdout to avoid clutter on the console, but it contains nothing interesting, so overwrite it with the result file.
+        timeout 1800 time xsb -e "[$pgm], xsbtest('__pycache__/$data.P'), halt."  1>>${outfile} 2>>${errfile}
+        if [ "$?" == "124" ]; then
+            echo "timeout!" >>xsb_result.txt
+        fi
+        mv -f xsb_result.txt $outfile
+        if ! grep -q "computing cputime" "$outfile"; then
+            echo "incomplete iteration; skipping remaining iterations"
+            break
+        fi
+    done
   done
 done
+
+# I usually run extract_times.py separately later, using run_extract.sh
+#iters=$(expr $endIter + 1)
+#python3 ../extract_times.py "PA" "${daPgms} ${xsbPgms}" "${datasets}" ${iters}
+
+echo " =============== FINISHED ==================="
+tput bel
