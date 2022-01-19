f591a9bde34555ee13523c3953d6bd6c5eb1f91b
Scott Stoller <stoller@cs.stonybrook.edu>
2022-01-19 16:35:13 -0500

    run.sh runs all RBAC programs on all datasets

--- examples/RBAC/run.sh
+++ examples/RBAC/run.sh
@@ -1,9 +1,59 @@
-for mode in RBACpy RBACda RBACunion RBACallloc RBACnonloc
-do
-  for i in $(seq 50 50 500)
-  do
-    timeout 1800 time python3.9 -m da --message-buffer-size=40960000 --rule test_paper.da --workloadcount $i --workloadmode random --mode $mode 1>>out.txt 2>>msg.txt
-    printf "end $mode-$i\n" >>out.txt 
-    printf "end $mode-$i\n" >>msg.txt
+#!/bin/bash
+
+# run this script in examples/RBAC
+
+# small workload for testing
+#pgms="RBACunion"
+#datasets="$(seq 50 50 500)"
+#startIter=0
+#endIter=2
+
+# run everything.
+pgms="RBACunion RBACallloc RBACnonloc RBACpy RBACda"
+datasets="$(seq 50 50 500)"
+startIter=0
+endIter=4
+
+if [ ! -d "out" ]; then
+    mkdir out
+fi
+
+for pgm in $pgms; do
+  for data in $datasets; do
+    for ((i=${startIter};i<=${endIter};i++)); do
+        outfile=out/${pgm}_${data}_${i}_out.txt
+        errfile=out/${pgm}_${data}_${i}_err.txt
+        if [ -a "$outfile" ]; then
+            rm -f $outfile
+        fi
+        if [ -a "$errfile" ]; then
+            rm -f $errfile
+        fi
+        echo "running $pgm $data $i"
+        timedout="false"
+        # if removing -I thread, add:  --message-buffer-size=409600000
+        timeout 1800 python3 -m da -r -I thread --rule RBACtimer.da --bench $pgm --workload $data --workmode random 1>>$outfile 2>>$errfile
+        # in case of timeout, append timeout message to outfile, to confirm that execution didn't abort for other reasons.
+        if [ "$?" == "124" ]; then
+            echo "timeout!" >>$outfile
+            timedout="true"
+        fi
+        if ! grep -q "run_os" "$outfile"; then
+            echo "incomplete iteration; skipping remaining iterations"
+            break
+        fi
+    done
+    # RBAC datasets are sorted smallest to largest.  in case of timeout, skip remaining datasets for this program.
+    if [ "$timedout" == "true" ]; then
+        echo "timeout: skipping remaining datasets for this program"
+        break
+    fi
   done
 done
+
+# I usually run extract_times.py separately later, using run_extract.sh
+#iters=$(expr $endIter + 1)
+#python3 ../extract_times.py "RBAC" "${pgms}" "${datasets}" ${iters}
+
+echo " =============== FINISHED ==================="
+tput bel
